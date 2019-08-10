e51a54e7bd8f98a5b63c01e4329dc297571f93af
seungin yang <rhfpdk92@naver.com>
2019-08-10 14:40:10 +0900

    슬랙 메세지 테스트3

--- src/main/java/com/giantdwarf/dockerjenkins/DockerJenkinsApplication.java
+++ src/main/java/com/giantdwarf/dockerjenkins/DockerJenkinsApplication.java
@@ -8,7 +8,7 @@ public class DockerJenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DockerJenkinsApplication.class, args);
-        System.out.println("슬랙에 메세지가 과연....");
+        System.out.println("슬랙에 메세지가 과연...3");
 
     }
 
