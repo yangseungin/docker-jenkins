d0b61c24deb6fea74fb6bbf5f7720b1878f25176
seungin yang <rhfpdk92@naver.com>
2019-08-10 13:25:49 +0900

    내용 수정후 github hook 확인

--- src/main/java/com/giantdwarf/dockerjenkins/DockerJenkinsApplication.java
+++ src/main/java/com/giantdwarf/dockerjenkins/DockerJenkinsApplication.java
@@ -8,7 +8,7 @@ public class DockerJenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DockerJenkinsApplication.class, args);
-        System.out.println("Hello");
+        System.out.println("내용 수정후 push");
 
     }
 
